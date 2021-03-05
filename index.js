cbb0e5c84270919c418dc2c450423804baf4c638
Nitish <nitishkumar25082000@gmail.com>
2021-03-06 03:18:53 +0530

    Setting Up Static Files Access

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
-
 app.use(expressLayouts);
 
+app.use(express.static('./assets'));
 // use express router
 app.use('/', require('./routes'));
 
