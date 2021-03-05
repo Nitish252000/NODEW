3f39662e31e98367d4de6ae228ac046273d21b45
Nitish <nitishkumar25082000@gmail.com>
2021-03-06 04:32:05 +0530

    Creating And Altering a Cookie

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 app.use(expressLayouts);
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+
 // extract style and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
